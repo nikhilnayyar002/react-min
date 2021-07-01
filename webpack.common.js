e686f2ad46e2bbaa27a644b945337c2533d0fc64
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2021-07-01 11:52:23 +0530

    feat(webpack): add plugin - detect circular depen.

--- webpack.common.js
+++ webpack.common.js
@@ -5,17 +5,18 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 const CleanTerminalPlugin = require('clean-terminal-webpack-plugin')
+const CircularDependencyPlugin = require('circular-dependency-plugin')
 const jsconfig = require('./jsconfig.json')
 
 let resoleAliasObj = {};
-(function() {
+(function () {
     let paths = jsconfig?.compilerOptions?.paths ?? {}
     for (let p in paths) {
         let a = p.substring(0, p.length - 2)
-        if(paths[p].length) {
+        if (paths[p].length) {
             let b = paths[p][0]
             let c = b.substring(0, b.length - 1)
-            resoleAliasObj[a]= path.resolve(__dirname, c)
+            resoleAliasObj[a] = path.resolve(__dirname, c)
         }
     }
 })();
@@ -30,7 +31,7 @@ module.exports = {
         assetModuleFilename: 'assets/[name].[contenthash].[ext]'
     },
     resolve: {
-        alias:resoleAliasObj
+        alias: resoleAliasObj
     },
     module: {
         rules: [
@@ -65,6 +66,19 @@ module.exports = {
             inject: 'body'
         }),
         // new webpack.EnvironmentPlugin(['APP_ID']),
+        new CircularDependencyPlugin({
+            // exclude detection of files based on a RegExp
+            exclude: /a\.js|node_modules/,
+            // include specific files based on a RegExp
+            include: /src/,
+            // add errors to webpack instead of warnings
+            failOnError: true,
+            // allow import cycles that include an asyncronous import,
+            // e.g. via import(/* webpackMode: "weak" */ './file.js')
+            allowAsyncCycles: false,
+            // set the current working directory for displaying module paths
+            cwd: process.cwd(),
+        }),
         new ESLintPlugin(),
         new CleanTerminalPlugin()
     ]
