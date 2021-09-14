45f87a7f98c2cb566b9c7fa15748cdc0b45e527c
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2021-09-14 20:38:34 +0530

    feat(typings): add index.d.ts for custom types
    we can now add assets like images with typescript import error

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+/** images */
+
+declare module "*.avif"
+declare module "*.gif"
+declare module "*.apng"
+declare module '*.png'
+declare module "*.jpg"
+declare module "*.jpeg"
+declare module "*.jfif"
+declare module "*.pjpeg"
+declare module "*.pjp"
+declare module "*.svg"
+declare module "*.webp"
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { unixEpochInMs } from './common';
+import sampleImg from './assets/sample.jpeg';
+import sampleJson from './assets/sample.json';
+
+console.log(sampleJson.hello)
 
 export default function App(): JSX.Element {
     const [state] = useState(unixEpochInMs)
@@ -8,6 +12,7 @@ export default function App(): JSX.Element {
         <div>
             <div>Hello WORLD</div>
             <div>{state}</div>
+            <img src={sampleImg} alt="" width="100" height="100" />
         </div>
     )
 }
