72cf4c625d37841e27578392be17d9c9d6c8fb5c
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2021-09-18 14:10:12 +0530

    feat(asset-txt): import raw text file content

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import svg from '@assets/react.svg';
 import Svg from '@assets/react.svg?react';
+import helloTxt from '@assets/hello.txt';
 
 const unixEpochInMs = Date.now()
 
@@ -16,7 +17,8 @@ function App() {
             <div>{state}</div>
             <img src={svg} width="200" height="200" />
             <div id="css-svg-sample"></div>
-            <Svg  width="200" height="200" viewBox="0 0 3500 3500"/>
+            <Svg width="200" height="200" viewBox="0 0 3500 3500" />
+            <pre>{helloTxt}</pre>
         </div>
     )
 }
