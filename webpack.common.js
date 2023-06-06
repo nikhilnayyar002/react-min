0e8ec1af9f9ca417214b6f275326a7faa8d1e7d2
Nikhil Nayyar <nikhilnayyar002@gmail.com>
2023-06-06 12:28:26 +0530

    feat: support npm linked libraries

--- webpack.common.js
+++ webpack.common.js
@@ -10,6 +10,7 @@ const ESLintPlugin = require("eslint-webpack-plugin");
 /********************************************************************* */
 
 module.exports = (/** @type {"development"|"production"} */ env) => ({
+  // cache: false, // set cache to false when live changes in linked libraries need to be reflected instantly in this running repo
   entry: path.resolve(__dirname, wmConfig.sourceDir, wmConfig.webpack.entryFilename),
   experiments: {
     outputModule: wmConfig.webpack.outputESModule, // https://webpack.js.org/configuration/output/#outputmodule
